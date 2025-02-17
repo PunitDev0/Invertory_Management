00f9eeb030548d592a9cc6b7fcf80afcc80dd69f
PunitDev0 <punitdeveloper1@gmail.com>
2025-02-17 16:09:20 +0530

    fixed some bugs

--- routes/web.php
+++ routes/web.php
@@ -24,83 +24,84 @@ Route::get('/logged-in-user', [AuthController::class, 'getLoggedInUser']);
 
 Route::group(['middleware' => 'auth'], function() {
     
-    Route::get('/admin/dashboard', function(){
-        return Inertia::render('Dashboard');
-    });
-    Route::get('/admin/category', function(){
-        return Inertia::render('Category');
-    });
-    Route::get('/admin/productname', function(){
-        return Inertia::render('ProductName');
-    });
-    Route::get('/admin/branches', function(){
-        return Inertia::render('Shop');
-    });
-    Route::get('/admin/allproducts', function(){
-        return Inertia::render('AllProducts');
-    });
-    
-    Route::get('/admin/product/{id?}', function( $id = null){
-        return Inertia::render('AddProduct', ['id' => $id]);
-    });
-    Route::get('/admin/user/{id?}', function( $id = null){
-        return Inertia::render('AddUser', ['id' => $id]);
-    });
-    Route::get('/admin/allusers', function(){
-        return Inertia::render('AllUsers');
-    });
-    Route::get('/admin/ordertracking', function(){
-        return Inertia::render('OrderTracking');
-    });
-    
-    
-    
-    Route::prefix('shops')->group(function () {
-    Route::get('/', [ShopController::class, 'index']); // GET /shops
-    Route::post('/', [ShopController::class, 'store']); // POST /shops
-    Route::put('/{id}', [ShopController::class, 'update']); // PUT /shops/{id}
-    Route::delete('/{id}', [ShopController::class, 'destroy']); // DELETE /shops/{id}
-    });
-    
-    Route::prefix('categories')->group(function () {
-    Route::post('/', [CategoryController::class, 'addCategory']); // Add category
-    Route::get('/', [CategoryController::class, 'getAllCategories']); // Get all categories
-    Route::get('{id}', [CategoryController::class, 'getCategory']); // Get a category by ID
-    Route::put('{id}', [CategoryController::class, 'updateCategory']); // Update category
-    Route::delete('{id}', [CategoryController::class, 'deleteCategory']); // Delete category
-    });
-    
-    // Product Name routes
-    Route::prefix('product-names')->group(function () {
-    Route::post('/', [ProductNameController::class, 'addProductName']); // Add a product name
-    Route::get('/', [ProductNameController::class, 'getAllProductNames']); // Get all product names
-    Route::get('{id}', [ProductNameController::class, 'getProductName']); // Get a product name by ID
-    Route::put('{id}', [ProductNameController::class, 'updateProductName']); // Update a product name by ID
-    Route::delete('{id}', [ProductNameController::class, 'deleteProductName']); // Delete a product name by ID
-    });
-    
-    Route::prefix('products')->group(function () {
-    Route::get('/', [ProductController::class, 'getAllProduct']);
-    Route::post('/add', [ProductController::class, 'addProduct']);
-    Route::put('/update/{id}', [ProductController::class, 'updateProduct']);
-    Route::delete('/delete/{id}', [ProductController::class, 'deleteProduct']);
-    });
-    
-    Route::prefix('roles')->group(function () {
-        Route::post('/create', [RoleController::class, 'store']);
-        Route::get('/list', [RoleController::class, 'getAllRoles']);
-    });
-    
-    // User Routes
-    Route::prefix('users')->group(function () {
-        Route::post('/register', [UserController::class, 'store']);
-        Route::get('/list', [UserController::class, 'getAllUsers']);
-        Route::delete('/delete/{id}', [UserController::class, 'deleteUser']);
-        Route::put('/update/{id}', [UserController::class, 'updateUser']);
-    });
-    
-    
-    Route::prefix('orders')->group(function () {
-        Route::get('/list', [OrderController::class, 'getAllOrders']);
-    });
+});
+
+Route::get('/admin/dashboard', function(){
+    return Inertia::render('Dashboard');
+});
+Route::get('/admin/category', function(){
+    return Inertia::render('Category');
+});
+Route::get('/admin/productname', function(){
+    return Inertia::render('ProductName');
+});
+Route::get('/admin/branches', function(){
+    return Inertia::render('Shop');
+});
+Route::get('/admin/allproducts', function(){
+    return Inertia::render('AllProducts');
+});
+
+Route::get('/admin/product/{id?}', function( $id = null){
+    return Inertia::render('AddProduct', ['id' => $id]);
+});
+Route::get('/admin/user/{id?}', function( $id = null){
+    return Inertia::render('AddUser', ['id' => $id]);
+});
+Route::get('/admin/allusers', function(){
+    return Inertia::render('AllUsers');
+});
+Route::get('/admin/ordertracking', function(){
+    return Inertia::render('OrderTracking');
+});
+
+
+
+Route::prefix('shops')->group(function () {
+Route::get('/', [ShopController::class, 'index']); // GET /shops
+Route::post('/', [ShopController::class, 'store']); // POST /shops
+Route::put('/{id}', [ShopController::class, 'update']); // PUT /shops/{id}
+Route::delete('/{id}', [ShopController::class, 'destroy']); // DELETE /shops/{id}
+});
+
+Route::prefix('categories')->group(function () {
+Route::post('/', [CategoryController::class, 'addCategory']); // Add category
+Route::get('/', [CategoryController::class, 'getAllCategories']); // Get all categories
+Route::get('{id}', [CategoryController::class, 'getCategory']); // Get a category by ID
+Route::put('{id}', [CategoryController::class, 'updateCategory']); // Update category
+Route::delete('{id}', [CategoryController::class, 'deleteCategory']); // Delete category
+});
+
+// Product Name routes
+Route::prefix('product-names')->group(function () {
+Route::post('/', [ProductNameController::class, 'addProductName']); // Add a product name
+Route::get('/', [ProductNameController::class, 'getAllProductNames']); // Get all product names
+Route::get('{id}', [ProductNameController::class, 'getProductName']); // Get a product name by ID
+Route::put('{id}', [ProductNameController::class, 'updateProductName']); // Update a product name by ID
+Route::delete('{id}', [ProductNameController::class, 'deleteProductName']); // Delete a product name by ID
+});
+
+Route::prefix('products')->group(function () {
+Route::get('/', [ProductController::class, 'getAllProduct']);
+Route::post('/add', [ProductController::class, 'addProduct']);
+Route::put('/update/{id}', [ProductController::class, 'updateProduct']);
+Route::delete('/delete/{id}', [ProductController::class, 'deleteProduct']);
+});
+
+Route::prefix('roles')->group(function () {
+    Route::post('/create', [RoleController::class, 'store']);
+    Route::get('/list', [RoleController::class, 'getAllRoles']);
+});
+
+// User Routes
+Route::prefix('users')->group(function () {
+    Route::post('/register', [UserController::class, 'store']);
+    Route::get('/list', [UserController::class, 'getAllUsers']);
+    Route::delete('/delete/{id}', [UserController::class, 'deleteUser']);
+    Route::put('/update/{id}', [UserController::class, 'updateUser']);
+});
+
+
+Route::prefix('orders')->group(function () {
+    Route::get('/list', [OrderController::class, 'getAllOrders']);
 });
